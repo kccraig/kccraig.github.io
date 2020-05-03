f196e7b6fe0982144e99b385deeef9d18d200e00
kccraig <kccraigdesigns@gmail.com>
2020-05-02 21:26:54 -0400

    year

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -1,3 +1,9 @@
 function lastupdate() {
     document.getElementById("updatebox").textContent = document.lastModified;
 }
+
+function getyear() {
+    var x = new Date();
+    var y = x.getFullYear();
+    document.getElementById("yearbox").innerHTML = y;
+}
