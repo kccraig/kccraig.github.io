c72403d76b7d387ffac22109deaa8dde52dc6fa6
kccraig <kccraigdesigns@gmail.com>
2020-05-02 22:49:07 -0400

    year

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -7,3 +7,8 @@ function getyear() {
     var y = x.getFullYear();
     document.getElementById("yearbox").innerHTML = y;
 }
+function getyear() {
+        var x = new Date();
+        var y = d.getFullYear();
+        document.getElementById("yearbox").innerHTML = x;
+}
