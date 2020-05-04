a04aa1fab73b62b0165f5581e1d3e1555c69d226
kccraig <kccraigdesigns@gmail.com>
2020-05-03 22:06:24 -0400

    currentyear

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+
 function lastupdate() {
-                document.getElementById("updatebox").textContent = document.lastModified;
+    document.getElementById("updatebox").textContent = document.lastModified;
+}
+
+function getyear() {
+    var x = new Date();
+    var y = x.getFullYear();
+    document.getElementById("currentyear").innerHTML = y;
+}
+
+function wrapper() {
+    lastupdate();
+    getyear();
 }
