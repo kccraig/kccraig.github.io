d7ad72e2f5a903ce6a243e303d2512c0d4300bca
kccraig <kccraigdesigns@gmail.com>
2020-05-14 22:10:24 -0400

    fix date

--- lesson3/js/scripts.js
+++ lesson3/js/scripts.js
@@ -8,8 +8,25 @@ function getyear() {
     var y = x.getFullYear();
     document.getElementById("currentyear").innerHTML = y;
 }
+function fullDay() {
+    var dy = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    
+    var n = weekday[dy.getDay()];
+    document.getElementById("dayName").innerHTML = n;
+    // <h1><span id="dayName">gotime</span></h1>
+    }
 
 function wrapper() {
+    fullDay();
     lastupdate();
     getyear();
+    
 }
