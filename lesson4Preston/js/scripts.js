27a4b71a9105a59666eab311dff179e7a6dbfc44
kccraig <kccraigdesigns@gmail.com>
2020-05-18 17:06:26 -0400

    date

--- lesson4Preston/js/scripts.js
+++ lesson4Preston/js/scripts.js
@@ -18,7 +18,26 @@ function getyear() {
     document.getElementById("currentyear").innerHTML = y;
 }
 
+function weekday() {
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+     var w = weekday[d.getDay()];
+     document.getElementById("dayoftheweek").innerHTML = w;
+    }
+
 function wrapper() {
     lastupdate();
     getyear();
+    toggleMenu();
+    weekday();
 }
+window.addEventListener('onload', wrapper());
+// Wednesday, 24 July 2020
