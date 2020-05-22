4307f7e30a110245b995ba48913ba4d2f6997036
kccraig <kccraigdesigns@gmail.com>
2020-05-21 20:03:49 -0400

    beautify

--- lesson5Preston/js/scripts.js
+++ lesson5Preston/js/scripts.js
@@ -5,40 +5,40 @@ function toggleMenu() {
     document.getElementById("primarynav").classList.toggle("hide");
 }
 
-    var x = new Date();
-    var y = x.getFullYear();
-    document.getElementsByClassName("currentyear")[0].innerHTML = y;
+var x = new Date();
+var y = x.getFullYear();
+document.getElementsByClassName("currentyear")[0].innerHTML = y;
 
-    var weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
-    var n = weekday[x.getDay()];
-   
-    var month = new Array();
-    month[0] = "January";
-    month[1] = "February";
-    month[2] = "March";
-    month[3] = "April";
-    month[4] = "May";
-    month[5] = "June";
-    month[6] = "July";
-    month[7] = "August";
-    month[8] = "September";
-    month[9] = "October";
-    month[10] = "November";
-    month[11] = "December";
-    var mon = month[x.getMonth()];
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+var n = weekday[x.getDay()];
+
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var mon = month[x.getMonth()];
 
 var fulldate = n + ", " + x.getDate() + " " + mon + " " + y;
 document.getElementById("thedate").innerHTML = fulldate;
 
 // function pancakes() {
 //     if (n == 5) {
-        
-// }
 
+
+// }
