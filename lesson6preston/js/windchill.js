9be117d7e97f9b252f20c8fdaad004c5b03708c4
kccraig <kccraigdesigns@gmail.com>
2020-05-29 22:12:27 -0400

    fix whind chill

--- lesson6preston/js/windchill.js
+++ lesson6preston/js/windchill.js
@@ -1,16 +1,13 @@
-
-
-
-var t= document.getElementById("prestemp").innerHTML;
+var t = document.getElementById("prestemp").innerHTML;
 var s = document.getElementById("wspeed").innerHTML;
 
-if (t < 50 && s < 3) {
 
+if (t < 50 && s > 3) {
+    var f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (.4275 * t * Math.pow(s, 0.16));
 
-var f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (.4275 * t * Math.pow(s, 0.16));
-var c = Math.round(f);
-document.getElementById("wchill").textContent = c + "°";
-} 
-else {
-    document.getElementById("wchill").textContent = "Nice Day" ;
+    var c = Math.round(f);
+    document.getElementById("wchill").textContent = c + "°";
+
+} else {
+    document.getElementById("wchill").textContent = "Nice Day";
 }
