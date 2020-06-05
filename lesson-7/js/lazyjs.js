378dd80acb37dbd0bd31d77f1d6ccdd661476aa4
kccraig <kccraigdesigns@gmail.com>
2020-06-05 18:39:40 -0400

    fixed and added

--- lesson-7/js/lazyjs.js
+++ lesson-7/js/lazyjs.js
@@ -1,30 +1,65 @@
 
-let imagesToLoad = document.querySelectorAll('img[data-src]');
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
+// let imagesToLoad = document.querySelectorAll('img[data-src]');
+// const loadImages = (image) => {
+//     image.setAttribute('src', image.getAttribute('data-src'));
+//     image.onload = () => {
+//         image.removeAttribute('data-src');
+//     };
+// };
+
+// imagesToLoad.forEach((img) => {
+//     loadImages(img);
+// });
+
+// if ('IntersectionObserver' in window) {
+//     const observer = new IntersectionObserver((items, observer) => {
+//         items.forEach((item) => {
+//             if (item.isIntersection) {
+//                 loadImages(item.target);
+//                 observer.unobserve(item.target);
+//             }
+//         });
+//     });
+//     imagesToLoad.forEach((img) => {
+//         observer.observe(img);
+//     });
+// } else {
+//     imagesToLoad.forEach((img) => {
+//         loadImages(img);
+//     });
+// }
+// get all imgs with data-src attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+// optional parameters being set for the IntersectionalObserver
+const imgOptions = {
+  threshold: 0.5
 };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
 
+// first check to see if Intersection Observer is supported
 if ('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if (item.isIntersection) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-        });
+  const imgObserver = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
     });
-    imagesToLoad.forEach((img) => {
-        observer.observe(img);
-    });
-} else {
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    });
+  }, imgOptions);
+
+  // loop through each img and check status and load if necessary
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+} else { // just load ALL images if not supported
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
 }
