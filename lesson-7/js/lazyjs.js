da433644ab335dc9c53ca23f127dff76db00f0b8
kccraig <kccraigdesigns@gmail.com>
2020-06-04 22:37:46 -0400

    lazyload

--- lesson-7/js/lazyjs.js
+++ lesson-7/js/lazyjs.js
@@ -1,3 +1,4 @@
+
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
@@ -7,21 +8,21 @@ const loadImages = (image) => {
 };
 
 imagesToLoad.forEach((img) => {
-loadImages(img);
+    loadImages(img);
 });
 
-if('IntersectionObserver' in window) {
-const observer = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
-        if(item.isIntersection) {
-            loadImages(item.target);
-            observer.unobserve(item.target);
-        }
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersection) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
     });
-});
-imagesToLoad.forEach((img) => {
-    observer.observe(img);
-});
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
 } else {
     imagesToLoad.forEach((img) => {
         loadImages(img);
