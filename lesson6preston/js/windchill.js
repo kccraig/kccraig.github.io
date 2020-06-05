a713013f38db56ca328404ee5ea0ad4040d9b17b
kccraig <kccraigdesigns@gmail.com>
2020-06-05 18:49:10 -0400

    go

--- lesson6preston/js/windchill.js
+++ lesson6preston/js/windchill.js
@@ -6,8 +6,8 @@ if (t < 50 && s > 3) {
     var f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (.4275 * t * Math.pow(s, 0.16));
 
     var c = Math.round(f);
-    document.getElementById("wchill").textContent = c + "°";
-
-} else {
-    document.getElementById("wchill").textContent = "Nice day, go outside!";
+    document.getElementById("wchill").innerHTML = c + "°";
+} 
+else {
+    document.getElementById("wchill").innerHTML = "Nice day, go outside!";
 }
