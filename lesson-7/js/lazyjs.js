4950034ee79a90e6f55f7e31aa6ccb172e4a2ba0
kccraig <kccraigdesigns@gmail.com>
2020-06-06 13:29:21 -0400

    fix js

--- lesson-7/js/lazyjs.js
+++ lesson-7/js/lazyjs.js
@@ -1,37 +1,6 @@
 
-// let imagesToLoad = document.querySelectorAll('img[data-src]');
-// const loadImages = (image) => {
-//     image.setAttribute('src', image.getAttribute('data-src'));
-//     image.onload = () => {
-//         image.removeAttribute('data-src');
-//     };
-// };
-
-// imagesToLoad.forEach((img) => {
-//     loadImages(img);
-// });
-
-// if ('IntersectionObserver' in window) {
-//     const observer = new IntersectionObserver((items, observer) => {
-//         items.forEach((item) => {
-//             if (item.isIntersection) {
-//                 loadImages(item.target);
-//                 observer.unobserve(item.target);
-//             }
-//         });
-//     });
-//     imagesToLoad.forEach((img) => {
-//         observer.observe(img);
-//     });
-// } else {
-//     imagesToLoad.forEach((img) => {
-//         loadImages(img);
-//     });
-// }
-// get all imgs with data-src attribute
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-// optional parameters being set for the IntersectionalObserver
 const imgOptions = {
   threshold: 0.5
 };
@@ -43,7 +12,7 @@ const loadImages = (image) => {
   };
 };
 
-// first check to see if Intersection Observer is supported
+
 if ('IntersectionObserver' in window) {
   const imgObserver = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
@@ -54,11 +23,10 @@ if ('IntersectionObserver' in window) {
     });
   }, imgOptions);
 
-  // loop through each img and check status and load if necessary
   imagesToLoad.forEach((img) => {
     imgObserver.observe(img);
   });
-} else { // just load ALL images if not supported
+} else { 
   imagesToLoad.forEach((img) => {
     loadImages(img);
   });
