a50c2127fa75619ebbe5752c5f774ec2e47d446e
kccraig <kccraigdesigns@gmail.com>
2020-06-16 20:19:00 -0400

    working

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -10,10 +10,21 @@ fetch(requestURL)
             for (let i = 0; i < prophets.length; i++) {
                 let card = document.createElement('section');
                 let h2 = document.createElement('h2');
+                let p = document.createElement('p');
+                let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
+                let image = document.createElement('img');
 
                 h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+                p.textContent = prophets[i].birthdate;
+                p2.textContent = prophets[i].birthplace;
+                p3.textContent = prophets[i].numofchildren;
+                image.setAttribute('src', prophets[i].imageurl);
 
-                card.appendChild(h2);
+                card.appendChild(h2);s
+                card.appendChild(p);
+                card.appendChild(p2);
+                card.appendChild(p3);
 
                 document.querySelector('div.cards').appendChild(card);
             }});
