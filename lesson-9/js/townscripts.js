d1d699a810770da88c3588e74ebbf67904681ae0
kccraig <kccraigdesigns@gmail.com>
2020-06-19 22:59:12 -0400

    finsihfooter working

--- lesson-9/js/townscripts.js
+++ lesson-9/js/townscripts.js
@@ -5,8 +5,8 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-            console.table(jsonObject); // temporary checking for valid response and data parsing
-            const prophets = jsonObject['towns'];
+            console.table(jsonObject); 
+            const towns = jsonObject['towns'];
             for (let i = 0; i < towns.length; i++) {
                 let towncard = document.createElement('section');
                 let h2 = document.createElement('h2');
@@ -14,7 +14,7 @@ fetch(requestURL)
                 let p2 = document.createElement('p');
                 let p3 = document.createElement('p');
                 let p4 = document.createElement('p');
-                let image = document.createElement('img');
+                // let image = document.createElement('img');
                 
 
 
