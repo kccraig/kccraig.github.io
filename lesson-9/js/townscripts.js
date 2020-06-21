59eeb7c59175cf397cf2ebf8de56b3139da95adf
kccraig <kccraigdesigns@gmail.com>
2020-06-20 21:38:36 -0400

    working

--- lesson-9/js/townscripts.js
+++ lesson-9/js/townscripts.js
@@ -1,5 +1,5 @@
-
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
 fetch(requestURL)
     .then(function (response) {
         return response.json();
@@ -7,11 +7,10 @@ fetch(requestURL)
     .then(function (jsonObject) {
             console.table(jsonObject); 
             const towns = jsonObject['towns'];
-            while towns[i].name === “Preston” || “Soda Springs” || “Fish Haven” {
-            
-                let div = document.createElement('div');
+            for (let i = 0; i < towns.length; i++) {
+                let town = document.createElement('section');
                 let h2 = document.createElement('h2');
-                let p = document.createElement('p');
+                let p1 = document.createElement('p');
                 let p2 = document.createElement('p');
                 let p3 = document.createElement('p');
                 let p4 = document.createElement('p');
@@ -19,27 +18,24 @@ fetch(requestURL)
 
 
                 h2.textContent = towns[i].name;
-                p.textContent = towns[i].motto;
+                p1.textContent = towns[i].motto;
                 p2.textContent = towns[i].yearFounded;
                 p3.textContent = towns[i].currentPopulation;
                 p4.textContent = towns[i].averageRainfall;
 
                 // image.setAttribute('src', prophets[i].imageurl);
-                // image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' ' + prophets[i].order);
+             
             
-                div.appendChild(h2);
-                div.appendChild(p);
-                div.appendChild(p2);
-                div.appendChild(p3);
-                div.appendChild(p4);
-                // div.appendChild(image); 
+                town.appendChild(h2);
+                town.appendChild(p1);
+                town.appendChild(p2);
+                // town.appendChild(image); 
 
-                document.querySelector('div.towninfo').appendChild(towninfo);
+                document.querySelector('div.towninfo').appendChild(town);
             }});
 
-            // h2=town name
-            // p = motto  
-            // p2 = year founded 
-            // p3 = population 
-            // p4 = annual rain fall  
-            // image  
+            // h2 town name
+            // p1 motto  
+            // p2 year founded  
+            // p3 population  
+            // p4 annual rain fall 
