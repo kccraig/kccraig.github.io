a497379feff8d1617b2e27f89b8cf2ff81b1862c
kccraig <kccraigdesigns@gmail.com>
2020-06-21 22:14:03 -0400

    fix pic

--- lesson-9/js/townscripts.js
+++ lesson-9/js/townscripts.js
@@ -9,14 +9,17 @@ fetch(requestURL)
             const towns = jsonObject['towns'];
             for (let i = 0; i < towns.length; i++) {
                 // if (towns[i].name == 'Preston' || 'Fish Haven' || 'Soda Springs') {
-                    if (i == 1 || 5 || 6) {
+                    // if (i == 1 || 5 || 6) {
+                if ((towns[i].name == 'Preston') || (towns[i].name == 'Fish Haven') || (towns[i].name =='Soda Springs')) {
+
                 let town = document.createElement('section');
                 let h2 = document.createElement('h2');
                 let p1 = document.createElement('p');
                 let p2 = document.createElement('p');
                 let p3 = document.createElement('p');
                 let p4 = document.createElement('p');
-                // let image = document.createElement('img');
+                let pic = document.createElement('img');
+                pic.setAttribute('src', "images/preston/smallcloudysky.jpg");
 
 
                 h2.textContent = towns[i].name;
@@ -24,6 +27,7 @@ fetch(requestURL)
                 p2.textContent = towns[i].yearFounded;
                 p3.textContent = towns[i].currentPopulation;
                 p4.textContent = towns[i].averageRainfall;
+                
 
                 // image.setAttribute('src', prophets[i].imageurl);
              
@@ -33,7 +37,7 @@ fetch(requestURL)
                 town.appendChild(p2);
                 town.appendChild(p3);
                 town.appendChild(p4);
-                // town.appendChild(image); 
+                town.appendChild(image); 
 
                 document.querySelector('div.towninfo').appendChild(town);
     }}});
