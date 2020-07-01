78104181c5564157a34541efc8e62bc7bf7f311a
kccraig <kccraigdesigns@gmail.com>
2020-06-30 20:18:34 -0400

    for now

--- lesson-10/js/tableweather.js
+++ lesson-10/js/tableweather.js
@@ -1,3 +1,32 @@
+
+/*CURRENT TIME*/
+var today = new Date();
+
+var hours = today.getHours();
+if (hours > 12) {
+  var h = (hours - 12);
+  var time = h + ":" + today.getMinutes() + "pm";
+} else {
+  time = hours + ":" + today.getMinutes() + "am";
+}
+// document.getElementById("nowtime").innerHTML = time;
+
+var x = new Date();
+// var y = x.getFullYear();
+// document.getElementsByClassName("currentyear")[0].innerHTML = y;
+
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+var n = weekday[x.getDay()];
+var d = x.getDay() + 1;
+
+
 const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=1c63b29616f73119eb969c661944c844&units=imperial';
 fetch(apiURL)
   .then((response) => response.json())
@@ -7,7 +36,7 @@ fetch(apiURL)
     // const imagesrc = `https://openweathermap.org/img/w/$jsObject.weather[0].icon)`;
     const fiveday = jsObject.list.filter(item => item.dt_txt.includes("18:00:00"));
     console.log(fiveday);
-    console.log(fiveday[1].main);
+    // console.log(fiveday[1].main);
 
     for (let i = 0; i < fiveday.length; i++) {
         let table = document.createElement('table');
@@ -16,7 +45,8 @@ fetch(apiURL)
         let tr2 = document.createElement('tr');
 
         let td1 = document.createElement('td');
-        let td2 = document.createAttribute('td');
+        let td2 = document.createElement('td');
+
         
         let image = document.createElement('img');
         var x = new Date();
@@ -24,18 +54,19 @@ fetch(apiURL)
         
        
         table.appendChild(tr);
-        table.appendChild(tr2);
         tr.appendChild(td1);
-        tr2.appendChild(td2);
+        tr.appendChild(td2);
+       
+        // table.appendChild(tr2);
         
-      
-        table.appendChild(tr2);
+        
+        
 
         // tr2.appendChild(td2);
         
         
-        td1.textContent = x;
-        td2.textContent = y;
+        td1.textContent = d;
+        td2.textContent = time;
       
         document.querySelector('div#table').appendChild(table);
         
