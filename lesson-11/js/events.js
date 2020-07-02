94fda5b6199df98b875d47efb79ecbc370ff5e58
kccraig <kccraigdesigns@gmail.com>
2020-07-02 15:42:34 -0400

    addevents

---
+++ lesson-11/js/events.js
@@ -0,0 +1,13 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+            console.table(jsonObject); 
+            const events = jsonObject['events'];
+            
+
+
+    }
