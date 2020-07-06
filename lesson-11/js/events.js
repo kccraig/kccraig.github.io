0842cb86e174663b73d314bf46c6f57d9627840c
kccraig <kccraigdesigns@gmail.com>
2020-07-05 22:24:44 -0400

    fix

--- lesson-11/js/events.js
+++ lesson-11/js/events.js
@@ -8,14 +8,18 @@ fetch(requestURL)
         console.table(jsonObject);
         const towns = jsonObject['towns'];
 
-        for (let i = 0; i <= towns[i].name.length; i++) 
+        for (let i = 0; i <= towns[i].name.length; i++) { 
 
                     if (towns[i].name == 'Soda Springs') {
+var x = towns[i].events[0];
+// for (x=0; x<=x.length; x++) {
+// alert(x);
+// }
 
-
+                           
 
 
                         
-               document.getElementById('eventbox').textContent = towns[i].events;
-
+              document.getElementById('eventbox').textContent = towns[i].events;
+                    }
     }})
