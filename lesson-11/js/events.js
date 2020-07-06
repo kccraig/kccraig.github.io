a0993455af8df18ed5184d510b18b8616017bf7e
kccraig <kccraigdesigns@gmail.com>
2020-07-05 21:45:43 -0400

    f

--- lesson-11/js/events.js
+++ lesson-11/js/events.js
@@ -5,36 +5,17 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-            console.table(jsonObject);
-            const towns = jsonObject['towns'];
+        console.table(jsonObject);
+        const towns = jsonObject['towns'];
 
-            for (let i = 0; i < towns.length; i++) {
-                if (towns[i].name == 'Soda Springs') {
-                    let m = towns[i].name;
-                    let eventbox = document.createElement('section');
-                    let h3 = document.createElement('h3');
-                    let event1 = document.createElement('p');
-                    let event2 = document.createElement('p');
+        for (let i = 0; i <= towns[i].name.length; i++) 
 
-                    h3.textContent = "Fun Events in" + " " + towns[i].name + " " + "this year";
-                    event1.textContent = towns[i].events;
-
-                }
-
-                eventbox.appendChild(h3);
-                eventbox.appendChild(event1);
+                    if (towns[i].name == 'Soda Springs') {
 
 
 
 
+                        
+               document.getElementById('eventbox').textContent = towns[i].events;
 
-
-                document.querySelector('section.eventbox').appendChild(eventbox);
-
-            }
-
-
-
-
-        }
-    })
+    }})
