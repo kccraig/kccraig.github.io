35dd7ba3c27eea81ecb538d351ff25ac9ed6d1b8
kccraig <kccraigdesigns@gmail.com>
2020-07-13 15:58:14 -0400

    fulljson

---
+++ finalproject/js/pricechart.js
@@ -0,0 +1,14 @@
+const requestURL = 'js/rentalprices.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+            console.table(jsonObject); 
+            const rentals = jsonObject['rentalpricechart'];
+            console.log(rentals);
+
+    }
+    )
+
