76ca2cf3d86d22ddc9256f582752dd9ccd21e334
kccraig <kccraigdesigns@gmail.com>
2020-07-17 19:51:16 -0400

    images

--- finalproject/js/pricechart.js
+++ finalproject/js/pricechart.js
@@ -8,9 +8,18 @@ fetch(requestURL)
             console.table(jsonObject); 
             const rentals = jsonObject['rentalpricechart'];
             console.log(rentals);
-         
-            }
+         for (let i=0; i<rentals.length; i++) {
+let scootcard = document.createElement('section');
+let h2 = document.createElement('h2');
 
-    
+h2.textContent = rentals[i].name;
+
+scootcard.appendChild(h2);
+
+document.querySelector(div.rentalpricechart).appendChild(scootcard);
+
+
+         }
+            }
     )
 
