7c372e6aa914c25ce7e8d40ac1f600787ea8fc14
kccraig <kccraigdesigns@gmail.com>
2020-07-16 21:50:07 -0400

    final

--- finalproject/js/cozweather.js
+++ finalproject/js/cozweather.js
@@ -12,37 +12,12 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
     cozinfo = jsObject;
-    const currentTemp = document.querySelector('.currenttemp');
-    currentTemp.textContent = Math.round(cozinfo.main.temp);
+    const currenttemp = document.querySelector('.currenttemp');
+    currenttemp.textContent = Math.round(cozinfo.main.temp);
 
     const humidity = document.querySelector('.humidity');
     humidity.textContent = Math.round(cozinfo.main.humidity);
 
-    // const imagesrc = `https://openweathermap.org/img/w/${cozinfo[o].weather[0].icon}.png`;
-    // const desc = fiveday[i].weather[0].description;
-
-    //  document.getElementsByClassName('wicon').src = imagesrc;
-    //  document.getElementsByClassName('wicon').alt = desc;
-
-
-  // const fiveday = cozinfo.list.filter(item => item.dt_txt.includes("12:00:00"));
-  // console.log(fiveday);
-  // console.log(fiveday[1].main);
-  // for (let i = 0; i < fiveday.length; i++) {
-  //   document.getElementsByClassName('day').textContent = fiveday[i].main.temp;
-  //   const imagesrc = `https://openweathermap.org/img/w/${fiveday[i].weather[0].icon}.png`;
-  //   const desc = fiveday[i].weather[0].description;
-  //   icon.setAttribute('src', imagesrc);
-  //   icon.setAttribute('alt', desc);
-
-  //   document.getElementsByClassName('ticon')[i].src = `https://openweathermap.org/img/w/${fiveday[i].weather[0].icon}.png`;
-  //   document.getElementsByClassName('ticon')[i].alt = desc;
-  //   if (d >= 7) {
-  //     d = 0;
-  //   }
-  //   document.getElementsByClassName('nextday')[i].textContent = weekday[d];
-  //   d++;
-  //   document.getElementsByClassName('nowtemp')[i].textContent = fiveday[i].main.temp;
-  // }
+  
 
 })
