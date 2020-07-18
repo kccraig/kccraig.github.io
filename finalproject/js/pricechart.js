342f34da84c9674e511a6d7c5d0939cfd0bd597b
kccraig <kccraigdesigns@gmail.com>
2020-07-17 22:49:38 -0400

    add elements

--- finalproject/js/pricechart.js
+++ finalproject/js/pricechart.js
@@ -14,19 +14,27 @@ fetch(requestURL)
             let image = document.createElement('img');
             let maxpeep = document.createElement('p');
             let rezpricehalfday = document.createElement('p');
+            let leftdiv = document.createElement('div');
+          
 
        
-
+            // var div = document.createElement('div');
+            // div.textContent = "Sup, y'all?";
+            // div.setAttribute('class', 'note');
+            // document.body.appendChild(div);
             image.setAttribute('src', rentals[i].imageurl);
             image.setAttribute('alt', rentals[i].name);
             maxpeep.textContent = "Maximum Riders " + rentals[i].maxpersons;
             rezpricehalfday.textContent = "Rezervation Price " + rentals[i].rezpricehalfday;
             h2.textContent = rentals[i].name;
+            leftdiv.textContent = "hi";
+            leftdiv.setAttribute('class','leftside');
 
             scootcard.appendChild(image); 
             scootcard.appendChild(h2);
             scootcard.appendChild(maxpeep);
             scootcard.appendChild(rezpricehalfday);
+            scootcard.appendChild(leftdiv);
         
             document.querySelector('div.rentalpricechart').appendChild(scootcard);
 
