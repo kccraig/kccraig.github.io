3cfceb5a955f90ae62f54b84651d65bc03bac278
kccraig <kccraigdesigns@gmail.com>
2020-07-18 17:50:40 -0400

    building table

--- finalproject/js/buildtable.js
+++ finalproject/js/buildtable.js
@@ -13,17 +13,21 @@ fetch(requestURL)
 
             var tbl = document.createElement('table');
             var tblBody = document.createElement('tbody');
-           alert(rentals.name);
-
+            var c = rentals[0];
+            alert(c.name);
             for (let i = 0; i < rentals.length; i++) {
 
                 var row = document.createElement('tr');
 
                 for (var j = 0; j < rentals.length; j++) {
-                    var cell = document.createElement('td');
-                    var cellText = document.createTextNode(rentals[j].name);
-                    cell.appendChild(cellText);
-                    row.appendChild(cell);
+                    var cell1 = document.createElement('td');
+                    var cell2 = document.createElement('td');
+                    var name = document.createTextNode(rentals[j].name);
+                    var maxper = document.createTextNode(rentals[j].maxpersons);
+                    cell1.appendChild(name);
+                    cell2.appendChild(maxper);
+                    row.appendChild(cell1);
+                    row.appendChild(cell2);
                 }
 
                 tblBody.appendChild(row);
