7f00d52d4255650327e334adcc98a1b9de9ffe1d
kccraig <kccraigdesigns@gmail.com>
2020-07-17 21:54:15 -0400

    section

--- finalproject/js/pricechart.js
+++ finalproject/js/pricechart.js
@@ -5,21 +5,31 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-            console.table(jsonObject); 
-            const rentals = jsonObject['rentalpricechart'];
-            console.log(rentals);
-         for (let i=0; i<rentals.length; i++) {
-let scootcard = document.createElement('section');
-let h2 = document.createElement('h2');
+        console.table(jsonObject);
+        const rentals = jsonObject['rentalpricechart'];
+        console.log(rentals);
+        for (let i = 0; i < rentals.length; i++) {
+            let scootcard = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let image = document.createElement('img');
+            let maxpeep = document.createElement('p');
+            let rezpricehalfday = document.createElement('p');
 
-h2.textContent = rentals[i].name;
+       
 
-scootcard.appendChild(h2);
+            image.setAttribute('src', rentals[i].imageurl);
+            image.setAttribute('alt', rentals[i].name);
+            maxpeep.textContent = "Maximum Riders " + rentals[i].maxpersons;
+            rezpricehalfday.textContent = "Rezervation Price " + rentals[i].rezpricehalfday;
+            h2.textContent = rentals[i].name;
 
-document.querySelector(div.rentalpricechart).appendChild(scootcard);
+            scootcard.appendChild(image); 
+            scootcard.appendChild(h2);
+            scootcard.appendChild(maxpeep);
+            scootcard.appendChild(rezpricehalfday);
+        
+            document.querySelector('div.rentalpricechart').appendChild(scootcard);
 
 
-         }
-            }
-    )
-
+        }
+    })
