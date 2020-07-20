f2f50f9455960e12e13b1881ace6de57e6f169d0
kccraig <kccraigdesigns@gmail.com>
2020-07-20 17:38:17 -0400

    fix object

--- finalproject/js/cozweather.js
+++ finalproject/js/cozweather.js
@@ -11,14 +11,32 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    cozinfo = jsObject;
+    const cozinfo = (jsObject);
     const currenttemp = document.querySelector('.currenttemp');
     currenttemp.textContent = Math.round(cozinfo.main.temp);
 
     const humidity = document.querySelector('.humidity');
     humidity.textContent = Math.round(cozinfo.main.humidity);
     
+    const fiveday = jsObject.list.filter(item => item.dt_txt.includes("18:00:00"));
+    console.log(fiveday);
+    console.log(fiveday[1].main);
+    for (let i = 0; i < fiveday.length; i++) {
+      document.getElementsByClassName('day').textContent = fiveday[i].main.temp;
+      const imagesrc = `https://openweathermap.org/img/w/${fiveday[i].weather[0].icon}.png`;
+      const desc = fiveday[i].weather[0].description;
+      icon.setAttribute('src', imagesrc);
+      icon.setAttribute('alt', desc);
 
+      document.getElementsByClassName('ticon')[i].src = `https://openweathermap.org/img/w/${fiveday[i].weather[0].icon}.png`;
+      document.getElementsByClassName('ticon')[i].alt = desc;
+      if (d >= 7) {
+        d = 0;
+      }
+      document.getElementsByClassName('nextday')[i].textContent = weekday[d];
+      d++;
+      document.getElementsByClassName('nowtemp')[i].textContent = fiveday[i].main.temp;
+    }
 
   
 
