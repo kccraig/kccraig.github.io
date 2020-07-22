0320dfc9963c5ee9ddb46a1f11624770be506ba3
kccraig <kccraigdesigns@gmail.com>
2020-07-22 19:57:18 -0400

    flowers

--- finalproject/js/cozweather.js
+++ finalproject/js/cozweather.js
@@ -24,24 +24,24 @@ fetch(apiURL)
     const feelslike = document.querySelector('.feelslike');
     feelslike.textContent = Math.round(cozinfo.main.feels_like);
     
-    // const fiveday = jsObject.list.filter(item => item.dt_txt.includes("18:00:00"));
-    // console.log(fiveday);
-    // console.log(fiveday[1].main);
-    // for (let i = 0; i < fiveday.length; i++) {
-    //   document.getElementsByClassName('day').textContent = fiveday[i].main.temp;
-    //   const imagesrc = `https://openweathermap.org/img/w/${fiveday[i].weather[0].icon}.png`;
-    //   const desc = fiveday[i].weather[0].description;
-    //   icon.setAttribute('src', imagesrc);
-    //   icon.setAttribute('alt', desc);
+    const fiveday = jsObject.list.filter(item => item.dt_txt.includes("18:00:00"));
+    console.log(fiveday);
+    console.log(fiveday[1].main);
+    for (let i = 0; i < fiveday.length; i++) {
+      document.getElementsByClassName('day').textContent = fiveday[i].main.temp;
+      const imagesrc = `https://openweathermap.org/img/w/${fiveday[i].weather[0].icon}.png`;
+      const desc = fiveday[i].weather[0].description;
+      icon.setAttribute('src', imagesrc);
+      icon.setAttribute('alt', desc);
 
-    //   document.getElementsByClassName('ticon')[i].src = `https://openweathermap.org/img/w/${fiveday[i].weather[0].icon}.png`;
-    //   document.getElementsByClassName('ticon')[i].alt = desc;
-    //   if (d >= 7) {
-    //     d = 0;
-    //   }
-    //   document.getElementsByClassName('nextday')[i].textContent = weekday[d];
-    //   d++;
-    //   document.getElementsByClassName('nowtemp')[i].textContent = fiveday[i].main.temp;
-    // }
+      document.getElementsByClassName('ticon')[i].src = `https://openweathermap.org/img/w/${fiveday[i].weather[0].icon}.png`;
+      document.getElementsByClassName('ticon')[i].alt = desc;
+      if (d >= 7) {
+        d = 0;
+      }
+      document.getElementsByClassName('nextday')[i].textContent = weekday[d];
+      d++;
+      document.getElementsByClassName('nowtemp')[i].textContent = fiveday[i].main.temp;
+    }
 
 })
