c6e1f8d0e106680c96de5fc08c0fb3d80b4f0037
kccraig <kccraigdesigns@gmail.com>
2020-07-22 20:04:40 -0400

    go

--- finalproject/js/cozweather.js
+++ finalproject/js/cozweather.js
@@ -8,7 +8,7 @@ if (hours > 12) {
   time = hours + ":" + today.getMinutes() + "am";
 }
 
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?lat=20.4757&lon=-86.9753&appid=1f7a3d0a7558f9b813673ce6add7859a&units=imperial';
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=20.4757&lon=-86.9753&appid=1f7a3d0a7558f9b813673ce6add7859a&units=imperial';
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
