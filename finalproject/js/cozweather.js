ab5eee76025105860069e335f216bb80812db1eb
kccraig <kccraigdesigns@gmail.com>
2020-07-23 11:34:09 -0400

    adding forecast

--- finalproject/js/cozweather.js
+++ finalproject/js/cozweather.js
@@ -1,5 +1,6 @@
 var today = new Date();
-
+var x = new Date();
+var y = x.getFullYear();
 var hours = today.getHours();
 if (hours > 12) {
   var h = (hours - 12);
@@ -8,21 +9,29 @@ if (hours > 12) {
   time = hours + ":" + today.getMinutes() + "am";
 }
 
-const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=20.4757&lon=-86.9753&appid=1f7a3d0a7558f9b813673ce6add7859a&units=imperial';
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+var n = weekday[x.getDay()];
+var d = x.getDay() + 1;
+
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=3530103&APPID=1f7a3d0a7558f9b813673ce6add7859a&units=imperial';
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    const cozinfo = (jsObject);
-  
+    const cozinfo = jsObject;
     const currenttemp = document.querySelector('.currenttemp');
-    currenttemp.textContent = Math.round(cozinfo.main.temp);
+    currenttemp.textContent = Math.round(cozinfo.list[0].main.temp);
+    const icon = document.querySelector('.ticon');
 
     const humidity = document.querySelector('.humidity');
-    humidity.textContent = Math.round(cozinfo.main.humidity);
-
-    const feelslike = document.querySelector('.feelslike');
-    feelslike.textContent = Math.round(cozinfo.main.feels_like);
+    humidity.textContent = Math.round(cozinfo.list[0].main.humidity);
     
     const fiveday = jsObject.list.filter(item => item.dt_txt.includes("18:00:00"));
     console.log(fiveday);
